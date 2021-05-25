3bdadad7c00675cf9c04271cf4224a30985ed7e7
Jamie Egan <j.egan@mac.com>
2021-05-25 18:40:19 +0100

    updating phone number

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,9 +5,8 @@ const Contact = () => {
     <div id="contact" className="container col-md-5 my-5 py-5">
       <h1 className="featurette-heading mt-5">Want to hire us?</h1>
       <h2 className="text-muted mb-5">
-        <span className="text-muted">Get in touch today.</span>{" "}
         <a href="mailto:contact@scarboroughcleaners.co.uk">Email us</a>{" "}
-        <span className="text-muted">or call </span> 07700000000.
+        <span className="text-muted">or call </span> 07534336064
       </h2>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="navbar fixed-top navbar-light bg-dark">
+    <nav className="navbar fixed-top navbar-light bg-primary">
       <div className="container-fluid">
         <span className="navbar-brand my-2 h1 text-white">
           Scarborough Cleaners
