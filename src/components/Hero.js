5befbc85021698cb3804a92d8646c7895910cd33
Jamie Egan <j.egan@mac.com>
2021-06-09 18:06:35 +0100

    fixing bug with button on safari

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -14,13 +14,13 @@ const Hero = () => {
           cleaning needs.
         </p>
         <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-          <a
+          <button
             href="#contact"
             type="button"
             className="btn btn-primary btn-lg px-4 gap-3"
           >
             Hire Us
-          </a>
+          </button>
         </div>
       </div>
     </div>
